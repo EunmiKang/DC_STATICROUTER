cb9867b3579f5e2b7d3851dcf29d2aeb65a9f191
Seongjun2 <tjdwns4302@naver.com>
2016-06-02 14:04:17 +0900

    device id

--- DC_ARP_7/Routing_Add_Dlg.cpp
+++ DC_ARP_7/Routing_Add_Dlg.cpp
@@ -6,6 +6,8 @@
 #include "Routing_Add_Dlg.h"
 #include "afxdialogex.h"
 #include "IpLayer.h"
+#include "Packet32.h"
+#include "afxmt.h"
 
 // Routing_Add_Dlg 대화 상자입니다.
 
@@ -32,6 +34,7 @@ void Routing_Add_Dlg::DoDataExchange(CDataExchange* pDX)
 	DDX_Check(pDX, c_gateway, check_gateway);
 	DDX_Check(pDX, c_host, check_host);
 	DDX_Control(pDX, e_metric, metric_hop);
+	DDX_Control(pDX, interfaceList, interface_List);
 }
 BOOL Routing_Add_Dlg::OnInitDialog(){
 
@@ -41,10 +44,81 @@ BOOL Routing_Add_Dlg::OnInitDialog(){
 	netmask_IP.SetWindowText("255.255.255.0");
 	Gateway_IP.SetWindowText("255.255.255.0");
 
+	UpdateData(TRUE);
+	
+	GetMacAddr();
 
 	return TRUE;
 }
 
+void Routing_Add_Dlg::GetMacAddr(){
+	
+	pcap_if_t *alldevs;
+    pcap_if_t *d;
+    int i=0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    
+    /* Retrieve the device list from the local machine */
+    if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL /* auth is not needed */, &alldevs, errbuf) == -1)
+    {
+        fprintf(stderr,"Error in pcap_findalldevs_ex: %s\n", errbuf);
+        exit(1);
+    }
+    
+    /* Print the list */
+    for(d= alldevs; d != NULL; d= d->next)
+    {
+        if (d->description)
+			interface_List.AddString(d->description);
+        else
+            printf(" (No description available)\n");
+    }
+    
+    if (i == 0)
+    {
+        printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
+        return;
+    }
+
+    /* We don't need any more the device list. Free it */
+    pcap_freealldevs(alldevs);
+			interface_List.AddString(m_Addr[i]->MacAddrDescrip);
+
+	
+}
+
+unsigned char* Routing_Add_Dlg::IpAddrStoN(char* IpString)
+{
+	unsigned char *result=(unsigned char*)malloc(sizeof(char)*4);	
+	char *temp=IpString;
+	char ip[4];
+	int n=0;	
+	
+	while(*temp!='\0'){	
+		while(*temp != '.' && *temp !='\0'){	//'.'단위로 나누어서 문자를 10진수숫자로 바꾼다.
+			ip[n++]= *temp;						//숫자로 바뀐값을 변수에 저장해서 리턴
+			temp++;
+		}
+		ip[n]='\0';		
+		n=0;
+		*result=(unsigned char)strtol(ip,NULL,10);
+		result++;
+		if(*temp=='\0'){
+			result-=4;							//4바이트 
+			return result;
+		}
+		temp++;
+	}
+	AfxMessageBox("IP주소 바꾸다가 에러 발생-_-;");
+	return NULL;
+}
+
+char* Routing_Add_Dlg::MacAddrNtoS(unsigned char *MAC)
+{
+	char *Result=(char*)malloc(sizeof(char)*20);
+	sprintf(Result,"%.2X:%.2X:%.2X:%.2X:%.2X:%.2X",MAC[0],MAC[1],MAC[2],MAC[3],MAC[4],MAC[5]);
+	return Result;
+}
 BEGIN_MESSAGE_MAP(Routing_Add_Dlg, CDialogEx)
 	ON_BN_CLICKED(add_Route_Table, &Routing_Add_Dlg::OnBnClickedaddRouteTable)
 
--- DC_ARP_7/Routing_Add_Dlg.h
+++ DC_ARP_7/Routing_Add_Dlg.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "afxcmn.h"
 #include "afxwin.h"
+#include "pcap.h"
+#include "ntddndis.h"
+#include "packet32.h"
 
 
 // Routing_Add_Dlg 대화 상자입니다.
@@ -12,7 +15,10 @@ class Routing_Add_Dlg : public CDialogEx
 public:
 	Routing_Add_Dlg(CWnd* pParent = NULL);   // 표준 생성자입니다.
 	virtual ~Routing_Add_Dlg();
-
+	LPMAC_ADDR m_Addr[MAX_ADAPTER_INFO];
+	unsigned char* IpAddrStoN(char* IpString);
+	char* MacAddrNtoS(unsigned char *MAC);
+	void GetMacAddr();
 // 대화 상자 데이터입니다.
 	enum { IDD = Routing_AddDlg };
 
@@ -29,7 +35,8 @@ public:
 	BOOL check_up;
 	BOOL check_gateway;
 	BOOL check_host;
-	CComboBox interfaceList;
 	CEdit metric_hop;
 
+
+	CComboBox interface_List;
 };
